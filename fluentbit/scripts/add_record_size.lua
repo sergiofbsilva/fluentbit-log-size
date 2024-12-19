8b4c8480a042089670e129354e66ab06660e48f4
Sérgio Silva <sergiofbsilva@gmail.com>
2024-12-19 23:32:42 +0000

    add non recursive log filter

--- fluentbit/scripts/add_record_size.lua
+++ fluentbit/scripts/add_record_size.lua
@@ -1,24 +1,32 @@
 function calculate_record_size(record)
-    local size = 0
+    local size, stack, i = 0, {record}, 1
 
-    local function calculate_size(value)
-        if type(value) == "string" then
-            -- string in bytes (considers the encoding)
-            return #value 
-        elseif type(value) == "number" then
-            return 8
-        elseif type(value) == "boolean" then
-            return 1
-        elseif type(value) == "table" then
-            return calculate_record_size(value)
-        else
-            -- ignore other types
-            return 0
+    while i > 0 do
+        local current = stack[i]
+        i = i - 1
+
+        for key, value in pairs(current) do
+            -- Add size of key
+            if type(key) == "string" then
+                size = size + #key
+            elseif type(key) == "number" then
+                size = size + 8
+            elseif type(key) == "boolean" then
+                size = size + 1
+            end
+
+            -- Add size of value
+            if type(value) == "string" then
+                size = size + #value
+            elseif type(value) == "number" then
+                size = size + 8
+            elseif type(value) == "boolean" then
+                size = size + 1
+            elseif type(value) == "table" then
+                i = i + 1
+                stack[i] = value -- Push nested table onto the stack
+            end
         end
-    end
-
-    for key, value in pairs(record) do
-        size = size + calculate_size(key) + calculate_size(value)
     end
 
     return size
