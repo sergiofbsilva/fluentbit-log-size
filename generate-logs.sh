e6041f7eaf21fa89490cdfc46975f7343e6b1881
Sérgio Silva <sergiofbsilva@gmail.com>
2024-12-19 23:12:42 +0000

    fix timestamp field

--- generate-logs.sh
+++ generate-logs.sh
@@ -7,6 +7,6 @@ generate_random_string() {
 while true; do
   str_size=$((RANDOM % (120 - 20 + 1) + 20))
   message=$(generate_random_string $str_size)
-  echo "{\"message\": \"$message\", \"number\":$str_size,  \"timestamp\": \"'$(date +%s)'\"}"  >> /var/log/dummy.log
+  echo "{\"message\": \"$message\", \"number\":$str_size,  \"timestamp\": \"$(date +%s)\"}"  >> /var/log/dummy.log
   sleep 1
 done
